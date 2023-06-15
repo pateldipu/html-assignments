41802f888afcd5723becb63dd20c420a350b6a66
patel dimpal <pateldimpal172003@gmail.com>
2023-06-15 10:52:26 +0530

    15-6

---
+++ javscript Task/externalfile.js
@@ -0,0 +1,12 @@
+h1 = document.getElementById('x');
+console.log(h1);
+
+// varname.style.property="value";
+
+// h1.style.color ="red";
+// h1.style.height ="200px";
+// h1.style.width ="200px";
+// h1.style.fontSize ="50px";
+// h1.style.background ="black"
+
+// h1.classList.add('xyz')
